ae32659c99eca3234b3a5deb174daea294058eba
Konstantin7553 <krasik150@gmail.com>
2024-07-31 16:22:22 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+
+result_fm = fm(10, 0)
+result_tm = tm(10, 0)
+
+
+print("result fm:", result_fm)
+print("result tm:", result_tm)
